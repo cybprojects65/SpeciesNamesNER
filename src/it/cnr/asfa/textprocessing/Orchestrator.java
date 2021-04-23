7350204805a21c108d6c4e2edadbd517af472bfe
gianpaolocoro <gianpaolo.coro@isti.cnr.it>
2021-04-23 11:32:34 +0200

    orchestratior updated

--- src/it/cnr/asfa/textprocessing/Orchestrator.java
+++ src/it/cnr/asfa/textprocessing/Orchestrator.java
@@ -5,37 +5,43 @@ import java.util.List;
 
 public class Orchestrator {
 //java -cp asfaspeciesner.jar it.cnr.asfa.textprocessing.Orchestrator sampleTextTaxa.txt
-	
+
 	@SuppressWarnings("null")
 	public static void main(String[] args) throws Exception {
 		System.out.println("ASFA Text Miner has started...");
 
-			
 		String annotationsParsed[] = null;
 		List<String> annotations = null;
 		String language = null;
-		File inputText = null; //NOTE: INPUT MUST BE UTF-8
+		File inputText = null; // NOTE: INPUT MUST BE UTF-8
 
 		if (args.length < 1) {
 			System.out.println("Using sample input");
-			args= new String[1];
-			args[0] = "sampleTextTaxa.txt";
+			args = new String[1];
+			args[0] = "textThesaurus.txt";
 		}
-		
-		
-		ASFAResearchObject researchObject = new ASFAResearchObject();
-		
 
-		researchObject.capture(args[0]);
-		researchObject.get();
-		researchObject.enrich("TAXON");
-		researchObject.materializeText();
-		researchObject.materializeJSON();
-		
+		ASFAResearchObjectSpecies researchObjectSpecies = new ASFAResearchObjectSpecies();
+		ASFAResearchObjectThesaurus researchObjectThesaurus = new ASFAResearchObjectThesaurus();
+
+//		Operazioni per Species
+		researchObjectSpecies.capture(args[0]);
+		researchObjectSpecies.get();
+		researchObjectSpecies.enrich("TAXON");
+		researchObjectSpecies.materializeText();
+		researchObjectSpecies.materializeJSON();
+		System.out.println("Operazioni species completate");
+
+//		Operazioni per Thesaurus
+		researchObjectThesaurus.capture(args[0]);
+		researchObjectThesaurus.get();
+		researchObjectThesaurus.enrich("THESAURUS");
+		researchObjectThesaurus.materializeText();
+		researchObjectThesaurus.materializeJSON();
+		System.out.println("Operazioni thesaurus completate");
+
 		System.out.println("Done");
 
-		
-		
 	}
 
 }
